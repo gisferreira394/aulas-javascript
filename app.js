9b040078aa5bcd590651d345034a4f9262200b82
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2022-10-04 22:27:17 -0300

    Estudo sobre variáveis e tipos de dados js

---
+++ app.js
@@ -0,0 +1,21 @@
+/*aula 01nde js*/
+/*
+Vamos trabalhar com:
+-Variáveis
+-Tipos de dados 
+-Entrada e saída de dados
+*/
+
+let data_nascimento
+let nome
+    let idade
+
+
+    /*
+    O js é fracamente tipado, isso significa que ao criar uma variável não é necessário informar o tipo. O tipoda variável será de acordo com o conteudo dela. Para criar uma variável utilizamos o comando let ou var ou const.
+
+    Os tipos de dados são:
+    - string: texto 
+   - number: números inteiros (int) ou reais ( float)
+    -boolean: boleano ( true / false )
+    */
